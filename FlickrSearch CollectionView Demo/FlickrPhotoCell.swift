52aa8ca521dbdcda8fa4696a0d49903f3fcdfa16
corwinc <corwincrownover@Corwins-MacBook-Air.local>
2016-05-03 11:23:12 -0700

    initial commit
    initial commit

---
+++ FlickrSearch CollectionView Demo/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch CollectionView Demo
+//
+//  Created by Corwin Crownover on 3/21/16.
+//  Copyright © 2016 Corwin Crownover. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    @IBOutlet weak var photoView: UIImageView!
+
+}
